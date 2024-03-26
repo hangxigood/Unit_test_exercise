971c436d6076b2b7fda6313df8be6131f19bf925
fernandocbhorta <151946784+fernandocbhorta@users.noreply.github.com>
2024-03-26 14:42:57 -0600

    Update Program.cs
    Added comments

--- Assiment3-Group10/Program.cs
+++ Assiment3-Group10/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// Assignment 3: Singly Linked Lists, Serialization and Testing
+// OOP-2 Using C#
+// Group Project
+// Members: Ahmed Obad, Fernando Horta, Hangxi Xiang
+// March 26th, 2024
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
